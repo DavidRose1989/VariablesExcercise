48fc5036df9b639a94c392bd20c6c6fa8b475210
DavidRose <DavidRose331@Gmail.com>
2023-01-31 09:48:58 -0600

    The Lion

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Declaring and Initialize variables
+
+string name = "Lion";
+int weight = 650;
+char middleInitial = 'S';
+bool isOver600 = true;
+double currentHeight = 36.5;
+decimal currentWeight = 650.99m;
+
+Console.WriteLine($"My favorite animal is a {name}, He weighs {weight} pounds and is very fat. He clearly eats too much.");
+Console.WriteLine();
+Console.WriteLine($"Here are his stats");
+Console.WriteLine();
+Console.WriteLine($"MiddleInitial: {middleInitial}");
+Console.WriteLine($"Is Over 600: {isOver600}");
+Console.WriteLine($"Current Height: {currentHeight}");
+Console.WriteLine($"Current Weight: {currentWeight}");
