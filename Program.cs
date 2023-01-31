a86b2d1546e140920d6a8a5ec0143766dfa6b7e5
DavidRose <DavidRose331@Gmail.com>
2023-01-31 10:34:34 -0600

    update notes

--- Program.cs
+++ Program.cs
@@ -2,16 +2,18 @@
 
 string name = "Lion";
 int weight = 650;
-char middleInitial = 'S';
+char sirInitial = 'S';
 bool isOver600 = true;
 double currentHeight = 36.5;
 decimal currentWeight = 650.99m;
 
-Console.WriteLine($"My favorite animal is a {name}, He weighs {weight} pounds and is very fat. He clearly eats too much.");
+Console.WriteLine($"My favorite animal is a {name}, he weighs {weight} pounds and is very fat. He clearly eats too much.");
+Console.WriteLine();
+Console.WriteLine($"The {name} has a {sirInitial} as his sir name, because he might be Simba the fat overweight Lion.");
+Console.WriteLine();
+Console.WriteLine($"What happened to you fatboy with last name Simba, heard you were over 600 pounds is that {isOver600}?");
 Console.WriteLine();
 Console.WriteLine($"Here are his stats");
 Console.WriteLine();
-Console.WriteLine($"MiddleInitial: {middleInitial}");
-Console.WriteLine($"Is Over 600: {isOver600}");
 Console.WriteLine($"Current Height: {currentHeight}");
 Console.WriteLine($"Current Weight: {currentWeight}");
